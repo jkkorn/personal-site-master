9e6094cc5dd1d22455e59f8a3ca8d7ff6977dc0b
Jonathan <jkkorn@gmail.com>
2019-05-21 01:45:36 -0300

    Revert "Revert "more changes""
    This reverts commit d9eae9f49f1d4ea9955f2f6ddcb674493755be2a.

--- app/data/projects.js
+++ app/data/projects.js
@@ -13,7 +13,28 @@ const data = [
     desc: 'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
       + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
       + 'and nutrient deficiencies at an affordable price for the developing world.',
-  }, {
+  }, 
+  {
+    title: 'Contributor for Nir Eyal and Ryan Hoover’s Hooked: How to Build Habit-Forming Products',
+    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
+    link: '/project/catdetector/',
+    image: '/images/projects/catdetector.jpg',
+    date: '2015-05-15',
+    desc: 'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
+     + 'Over 60,000 cats were classified before server bills made the project too expensive '
+     + 'to continue hosting.',
+  },
+  {
+    title: 'Two time finalist at Health 2.0’s hackathon ’10 andʼ11',
+    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
+    link: '/project/catdetector/',
+    image: '/images/projects/catdetector.jpg',
+    date: '2015-05-15',
+    desc: 'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
+     + 'Over 60,000 cats were classified before server bills made the project too expensive '
+     + 'to continue hosting.',
+  },
+  {
     title: 'City Planning tool',
     subtitle: 'Winner of Google\'s Hack4Health hackathon',
     link: 'http://www.spacepotato.org',
@@ -23,7 +44,7 @@ const data = [
       + 'and gps transponder. Resulting photos were published in a coffee table book. '
       + 'You can email me for a copy.',
   }, {
-    title: 'Cat Detector',
+    title: 'Winner of University of San Franciscoʼs Magis leadership award \ʼ09',
     subtitle: 'A convolutional neural network to classify cats! (and dogs)',
     link: '/project/catdetector/',
     image: '/images/projects/catdetector.jpg',
@@ -32,6 +53,17 @@ const data = [
      + 'Over 60,000 cats were classified before server bills made the project too expensive '
      + 'to continue hosting.',
   },
+  {
+    title: 'Semfinalist Amazon\'s startup challenge',
+    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
+    link: '/project/catdetector/',
+    image: '/images/projects/catdetector.jpg',
+    date: '2015-05-15',
+    desc: 'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
+     + 'Over 60,000 cats were classified before server bills made the project too expensive '
+     + 'to continue hosting.',
+  },
+  
 ];
 
 export default data;
